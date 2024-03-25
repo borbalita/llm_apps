18dd97aaf39e6be8f047cb273284849d9bffd841
Borbala Tasnadi <borbala.tasnadi@gmail.com>
2024-03-25 11:49:50 +0100

    create chat with sqlite memory

--- pdf/app/chat/chat.py
+++ pdf/app/chat/chat.py
@@ -1,4 +1,8 @@
+from app.chat.llms.chat_openai import build_llm
+from app.chat.memories.sql import build_memory
 from app.chat.models import ChatArgs
+from app.chat.vectore_stores.pinecone import build_retriever
+from langchain.chains import ConversationalRetrievalChain
 
 
 def build_chat(chat_args: ChatArgs):
@@ -12,5 +16,11 @@ def build_chat(chat_args: ChatArgs):
 
         chain = build_chat(chat_args)
     """
-
-    pass
+    retriever = build_retriever(chat_args)
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        memory=memory,
+        retriever=retriever
+    )
